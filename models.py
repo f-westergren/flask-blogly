d4c92270c0a198b8978e8b1dab1f240f05d11e9c
f-westergren <folke.westergren@gmail.com>
2020-05-12 09:30:58 -0400

    working property solution

--- models.py
+++ models.py
@@ -13,25 +13,13 @@ def connect_db(app):
 class User(db.Model):
     __tablename__ = 'users'
 
-    def __init__(self, first_name, last_name, image_url=DEFAULT_IMAGE_URL):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.image_url = image_url
-        self.full_name = f'{self.first_name} {self.last_name}'
-        
-        print(self.first_name, self.last_name)
-
     def __repr__(self):
         u = self
         return f"<User id={u.id} first_name={u.first_name} last_name={u.last_name}>"
     
     @property
     def full_name(self):
-        return self._full_name
-
-    @full_name.setter
-    def full_name(self, value):
-        self._full_name = value
+        return f'{self.first_name} {self.last_name}'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     first_name = db.Column(db.String(30), nullable=False)
